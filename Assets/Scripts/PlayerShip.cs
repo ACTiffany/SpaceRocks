d1f83111c24594fbddb5543be9ebc4360d7b7ad7
HCTiffany <HCTiffany@gmail.com>
2021-08-15 12:33:01 -0400

    -Updated project to New Input Syttem.

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerShip : MonoBehaviour
 {
 	[SerializeField]
 	private float thrust;
 	[SerializeField]
-	private float turnRate;
-	[SerializeField]
 	private float maxVelocity;
 
 	[SerializeField]
+	private float turnRate;
+	[SerializeField]
+	private float maxTurnRate;
+
+
+	[SerializeField]
 	private Rigidbody2D physicsBody;
+
+	// Active movement Values
+	private bool isAccelerating = false;
+	private Vector2 currentAcceleration;
+	
+	private bool isTurning = false;
+	private float currentTurn;
 
 	// Start is called before the first frame update
 	void Start()
@@ -24,17 +38,46 @@ public class PlayerShip : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		if (physicsBody.velocity.magnitude < maxVelocity)
-		{
-			if (Input.GetKey(KeyCode.W))
-				physicsBody.AddRelativeForce(new Vector2(0, thrust));
-		}
-		if (Input.GetKey(KeyCode.A))
-			physicsBody.AddTorque(turnRate);
-		if (Input.GetKey(KeyCode.D))
-			physicsBody.AddTorque(-turnRate);
+		HandleMovement();
+	}
+
+	// Movement Controls
+	private void HandleMovement()
+	{
+		if (isAccelerating)
+			physicsBody.AddRelativeForce(currentAcceleration);
+
+		if (isTurning)
+			physicsBody.AddTorque(-currentTurn);
 
 		physicsBody.velocity = Vector2.ClampMagnitude(physicsBody.velocity, maxVelocity);
+		physicsBody.angularVelocity = Mathf.Clamp(physicsBody.angularVelocity, -maxVelocity, maxVelocity);
 
+	}
+	public void OnThrust(InputAction.CallbackContext value)
+	{
+		if (value.started)
+		{
+			isAccelerating = true;
+			currentAcceleration = value.ReadValue<Vector2>() * new Vector2(0, 1 * thrust);
+		}
+
+		if (value.canceled)
+		{
+			isAccelerating = false;
+		}
+	}
+	public void OnTurn(InputAction.CallbackContext value)
+	{
+		if (value.started)
+		{
+			isTurning = true;
+			currentTurn = value.ReadValue<Vector2>().x * turnRate;
+		}
+
+		if (value.canceled)
+		{
+			isTurning = false;
+		}
 	}
 }
